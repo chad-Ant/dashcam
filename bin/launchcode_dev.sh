9a1a3cd678e688a8922a21a5e878f87940f70ae0
chad-Ant <53463752+chad-Ant@users.noreply.github.com>
2026-03-29 08:31:09 +0700

    Add files via upload

---
+++ bin/launchcode_dev.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+IMAGE_NAME="l4t-ml-gpio"
+INIT_DIR="/user/dashcam"
+OUTPUT_DIR="/media/$USER/backup"
+
+echo "Booting Machine Vision Environment: $IMAGE_NAME..."
+
+docker run -it --rm --network=host --privileged --ipc=host \
+    -w $INIT_DIR \
+    -v /dev:/dev \
+    -v /tmp/argus_socket:/tmp/argus_socket \
+    -v $OUTPUT_DIR:/user/output \
+    -v /home/$USER/dashcam:/user/dashcam \
+    $IMAGE_NAME
